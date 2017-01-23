19d3ed9c3fc73fc2e6052ea3326d3f256b06f574
saposki <saposki@gmail.com>
2017-01-23 14:49:27 -0800

    built-in-factory complete

--- build/snapterest.js
+++ build/snapterest.js
@@ -18739,20 +18739,13 @@ if (process.env.NODE_ENV !== 'production') {
 },{"./lib/Object.assign":50,"./lib/React":52,"./lib/deprecated":131,"_process":27}],156:[function(require,module,exports){
 var React = require('react');
 var ReactDOM = require('react-dom');
-// var reactElement = React.createElement('h1');
 
-// var h1 = React.createElement('h1', {className: 'header', key:'header'}, 'This is React');
-// var p = React.createElement('p', {className:'content', key:'content'}, "And that's how it works.");
-// var reactFragment = [h1, p];
-// var section = React.createElement('section', {className:'container'}, reactFragment)
-
-var listItemElement1 = React.createElement('li', { className: 'item-1', key: 'item-1' }, 'Item-1');
-var listItemElement2 = React.createElement('li', { className: 'item-2', key: 'item-2' }, 'Item-2');
-var listItemElement3 = React.createElement('li', { className: 'item-3', key: 'item-3' }, 'Item-3');
+var listItemElement1 = React.DOM.li({ className: 'item-1', key: 'item-1' }, 'Item-1');
+var listItemElement2 = React.DOM.li({ className: 'item-2', key: 'item-2' }, 'Item-2');
+var listItemElement3 = React.DOM.li({ className: 'item-3', key: 'item-3' }, 'Item-3');
 
 var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
-var listOfItems = React.createElement('ul', { className: 'list-of-items' }, reactFragment);
-
+var listOfItems = React.DOM.ul({ className: 'list-of-items' }, reactFragment);
 ReactDOM.render(listOfItems, document.getElementById('react-application'));
 
 },{"react":155,"react-dom":28}]},{},[156]);
--- source/app.js
+++ source/app.js
@@ -1,26 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-// var reactElement = React.createElement('h1');
 
-// var h1 = React.createElement('h1', {className: 'header', key:'header'}, 'This is React');
-// var p = React.createElement('p', {className:'content', key:'content'}, "And that's how it works.");
-// var reactFragment = [h1, p];
-// var section = React.createElement('section', {className:'container'}, reactFragment)
-//
-// var listItemElement1 = React.createElement('li', {className:'item-1', key:'item-1'}, 'Item-1');
-// var listItemElement2 = React.createElement('li', {className:'item-2', key:'item-2'}, 'Item-2');
-// var listItemElement3 = React.createElement('li', {className:'item-3', key:'item-3'}, 'Item-3');
-//
-//
-// var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
-// var listOfItems = React.createElement('ul', {className: 'list-of-items'}, reactFragment);
-
-var createListItemElement = React.createFactory('li');
-
-var listItemElement1 = React.createListItemElement({className:'item-1', key:'item-1'}, 'Item-1');
-var listItemElement2 = React.createListItemElement({className:'item-2', key:'item-2'}, 'Item-2');
-var listItemElement3 = React.createListItemElement({className:'item-3', key:'item-3'}, 'Item-3');
+var listItemElement1 = React.DOM.li({className:'item-1', key:'item-1'}, 'Item-1');
+var listItemElement2 = React.DOM.li({className:'item-2', key:'item-2'}, 'Item-2');
+var listItemElement3 = React.DOM.li({className:'item-3', key:'item-3'}, 'Item-3');
 
 var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
-
+var listOfItems = React.DOM.ul({className:'list-of-items'}, reactFragment)
 ReactDOM.render(listOfItems, document.getElementById('react-application'));
